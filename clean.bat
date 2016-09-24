b1643545695882e839217228b0267079ad0b9d67
Rene Tapia <rtapiapincheira@gmail.com>
2016-09-24 05:46:05 -0330

    Completa sanitizador, solo falta manejar el caso en que un campo de una linea sea numerico (no va encerrado en comillas y ahi falla al detectar lineas diferentes)

--- clean.bat
+++ clean.bat
@@ -1,3 +1,11 @@
 @echo off
-python scripts\cleaner.py data\Clientes.txt temp\clients.txt
+python scripts\cleaner.py "data\Categorias.txt" "temp\categories.txt"
+python scripts\cleaner.py "data\Clientes.txt" "temp\clients.txt"
+python scripts\cleaner.py "data\Companias de envios.txt" "temp\shippingCompanies.txt"
+python scripts\cleaner.py "data\Detalles de pedidos.txt" "temp\orderDetails.txt"
+python scripts\cleaner.py "data\Empleados.txt" "temp\employees.txt"
+python scripts\cleaner.py "data\Pedidos.txt" "temp\orders.txt"
+python scripts\cleaner.py "data\Productos.txt" "temp\products.txt"
+python scripts\cleaner.py "data\Proveedores.txt" "temp\providers.txt"
 @echo on
+
--- scripts/cleaner.py
+++ scripts/cleaner.py
@@ -1,44 +1,45 @@
-
+import re
 import sys
 import os
 
 def clean_file(inputfile, outputfile):
-	print 'Reading from %s' % inputfile
-	print 'Writing to %s' % outputfile
+    print 'Reading from %s' % inputfile
+    print 'Writing to %s' % outputfile
 
-	if not os.path.isfile(inputfile):
-		print 'the file %s doesnt exist' % inputfile
-		return
+    if not os.path.isfile(inputfile):
+        print 'the file %s doesnt exist' % inputfile
+        return
 
-	memfile = open(inputfile).read()
+    memfile = open(inputfile).read()
 
-	lines = memfile.split('\n');
+    while ';\n' in memfile:
+        memfile = memfile.replace(';\n', ';""\n')
+    
 
-	def clean_delimiters(s):
-		return s.strip(' \t\n\r\b"')
+    L = [l.replace('\n',' ').strip() for l in memfile.split('"\n')]
+    
+    def clean_delimiters(s):
+        return s.strip(' \t\n\r\b"')
 
-	header = lines[0]
-	parts = [clean_delimiters(e) for e in header.split(';')]
+    header = [clean_delimiters(e) for e in L[0].split(';')]
 
-	del(lines[0])
-	merged = ' '.join([l.strip() for l in lines])
-	
-	tokens = [clean_delimiters(e) for e in merged.split(';')]
+    del(L[0])
+    
+    LS = [[clean_delimiters(e) for e in k.split(';')] for k in L]
 
-	a = len(tokens)
-	b = len(parts)
-	if a % b != 0:
-		print 'detected a=%d columns, but there are b=%d data fields in the body (a should divide b)' % (b, a)
-		exit()
+    h = open(outputfile, 'w')
+    h.write(';'.join(header))
+    for l in LS:
+    	h.write('\n')
+    	h.write(';'.join(l))
+    h.close()
 
-	print parts
-	print tokens
 
 def print_help():
-	print 'python cleaner.py <input file> <output file>'
+    print 'python cleaner.py <input file> <output file>'
 
 if __name__ == '__main__':
-	if len(sys.argv[1:]) == 2:
-		clean_file(sys.argv[1], sys.argv[2])
-	else:
-		print_help()
+    if len(sys.argv[1:]) == 2:
+        clean_file(sys.argv[1], sys.argv[2])
+    else:
+        print_help()
